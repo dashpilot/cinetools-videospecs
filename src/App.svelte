2c950db08aaeb6e4ef6d0d22fdb7ec76c579f316
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-12-08 11:33:06 +0000

    Updated logo

--- src/App.svelte
+++ src/App.svelte
@@ -74,7 +74,7 @@
 
 <a href="https://www.cinetools.net"
   ><img
-    src="https://www.cinetools.net/img/videospecs.png"
+    src="https://www.cinetools.net/img/banner-videospecs.png"
     id="logo"
 /></a>
 <div class="wrap">
